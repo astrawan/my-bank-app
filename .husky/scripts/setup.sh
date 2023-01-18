aec527c24b0de1a9291e086fdefda822c9bbe79d
Astrawan <astrawan@icloud.com>
2023-01-18 15:31:49 +0800

    Add template checksum to README.md

--- .husky/scripts/setup.sh
+++ .husky/scripts/setup.sh
@@ -4,20 +4,38 @@ SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
 PROJECT_HOOKS_DIR="$(dirname "$SCRIPT_DIR")"
 PROJECT_DIR="$(dirname "$PROJECT_HOOKS_DIR")"
 PROJECT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-PROJECT_UPDATE_STATUS=0
+PROJECT_README_UPTODATE=0
+PROJECT_README_CHECKSUM=$(sha1sum "$PROJECT_DIR/templates/README.ejs")
 
 if [ -f "$PROJECT_DIR/README.md" ];
 then
-  PROJECT_UPDATE_STATUS=$(grep -c "<\!-- branch: $PROJECT_BRANCH -->" "$PROJECT_DIR/README.md") || true
-  if [ "$PROJECT_UPDATE_STATUS" -eq 1 ];
+  PROJECT_README_UPTODATE=$(grep -c "<\!-- template-checksum: $PROJECT_README_CHECKSUM -->" "$PROJECT_DIR/README.md") || true
+  if [ "$PROJECT_README_UPTODATE" -eq 1 ];
   then
     modified=$(git diff --name-only --cached "$PROJECT_DIR/README.md" | wc -l) || true
     if [ "$modified" -eq 1 ];
     then
-      modified=$(git diff --cached "$PROJECT_DIR/README.md" | grep -c "+<\!-- branch: $PROJECT_BRANCH -->") || true
+      modified=$(git diff --cached "$PROJECT_DIR/README.md" | grep -c "+<\!-- template-checksum: $PROJECT_README_CHECKSUM -->") || true
       if [ "$modified" -eq 1 ];
       then
-        PROJECT_UPDATE_STATUS=0
+        PROJECT_README_UPTODATE=0
+      fi
+    fi
+  fi
+
+  if [ "$PROJECT_README_UPTODATE" -eq 1 ];
+  then
+    PROJECT_README_UPTODATE=$(grep -c "<\!-- branch: $PROJECT_BRANCH -->" "$PROJECT_DIR/README.md") || true
+    if [ "$PROJECT_README_UPTODATE" -eq 1 ];
+    then
+      modified=$(git diff --name-only --cached "$PROJECT_DIR/README.md" | wc -l) || true
+      if [ "$modified" -eq 1 ];
+      then
+        modified=$(git diff --cached "$PROJECT_DIR/README.md" | grep -c "+<\!-- branch: $PROJECT_BRANCH -->") || true
+        if [ "$modified" -eq 1 ];
+        then
+          PROJECT_README_UPTODATE=0
+        fi
       fi
     fi
   fi
@@ -26,4 +44,5 @@ fi
 export PROJECT_BRANCH
 export PROJECT_DIR
 export PROJECT_HOOKS_DIR
-export PROJECT_UPDATE_STATUS
+export PROJECT_README_CHECKSUM
+export PROJECT_README_UPTODATE
