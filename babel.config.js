fa910a3f2e1afdbbd97b5045c810b001a57841a3
Astrawan Wayan <astra@pm.me>
2025-08-05 19:23:15 +0800

    chore(version): update dependency packages

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,6 @@ module.exports = (api) => {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: ['react-native-worklets/plugin'],
   };
 };
